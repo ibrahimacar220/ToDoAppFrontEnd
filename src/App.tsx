af8905d79c903b422ae442683feec54d792c5a10
ibrahimacar220 <iacar8800@gmail.com>
2023-10-27 20:24:22 +0300

    i added toast alerts and some login and error controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './Pages/login';
-import Nav from './Components/nav'
+import Nav from './Components/nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Navigate, useNavigate } from "react-router-dom";
 import Home from './Pages/Home';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './Pages/login';
 import Register from './Pages/Register';
 
 function App() {
-  
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav></Nav>
+        <Nav />
         <main className="form-signin">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -23,9 +22,9 @@ function App() {
           </Routes>
         </main>
       </BrowserRouter>
+      <ToastContainer />
     </div>
   );
 }
-
 
 export default App;
